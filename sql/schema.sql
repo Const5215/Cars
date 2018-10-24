b16d69286d5da812723a5bfff0907c26eb16d972
hwang75 <hwang75@ncsu.edu>
2018-10-24 14:48:03 -0400

    fix typo

--- sql/schema.sql
+++ sql/schema.sql
@@ -176,7 +176,7 @@ create table CAR_MODEL
 
 create table CAR
 (
-  PALTE             VARCHAR2(255) not null
+  PLATE             VARCHAR2(255) not null
     constraint CAR_PK
     primary key,
   CUSTOMER_ID       NUMBER        not null
